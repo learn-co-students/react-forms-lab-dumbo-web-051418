2137ecb72a09b73c4bf3216ae5a85685a34343f1
prmoreira23 <prm.gredes@gmail.com>
2018-07-17 17:30:24 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,47 @@
 import React from "react";
 
 class PoemWriter extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      text: "",
+      valid: true
+    };
+  }
+
+  isValidPoem = poem => {
+  const poemLines = poem.split("\n").map(line => line.trim());
+  const isRightAmountOfLines = poemLines.length === 3;
+  if (poem && isRightAmountOfLines) {
+    return (
+      poemLines[0].split(" ").length === 5 &&
+      poemLines[1].split(" ").length === 3 &&
+      poemLines[2].split(" ").length === 5
+    );
+  } else {
+    return false;
+  }
+};
+
+  onChange = event => {
+    let oldState = {...this.state};
+
+    oldState.text = event.target.value;
+    oldState.valid = this.isValidPoem(oldState.text);
+    this.setState({
+      ...oldState,
+    });
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" onChange={this.onChange} value={this.state.text}/>
+        {!this.state.valid && (<div id="poem-validation-error" style={{ color: "red" }}>
+            This poem is not written in the right structure!
+          </div>)
+        }
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value,
+    }, function(){
+
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <p>Remaining characters: {this.props.maxChars - this.state.text.length}</p>
       </div>
     );
   }
