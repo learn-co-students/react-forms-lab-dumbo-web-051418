82d1840ba0e1334ff0cd58e37911b5a224f5d82f
pablopawpaw <lpault29@gmail.com>
2018-07-17 21:48:53 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,46 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textAreaInput: "",
+      isValid: true,
+    };
+  }
+
+  isValidPoem = poem => {
+    const poemLines = poem.split("\n").map(line => line.trim());
+    const isRightAmountOfLines = poemLines.length === 3;
+    const reqNumOfWords = poemLines[0].split(" ").length === 5 &&
+          poemLines[1].split(" ").length === 3 &&
+          poemLines[2].split(" ").length === 5
+    return isRightAmountOfLines && reqNumOfWords
+  }
+
+  handleTextArea = (e) => {
+    this.setState({
+      textAreaInput: e.target.value,
+      isValid: this.isValidPoem(e.target.value)
+    })
+  }
+
+  renderError = () => {
+    return (
+      <div id="poem-validation-error" style={{ color: "red" }}>
+        This poem is not written in the right structure!
+      </div>
+    )
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60"
+          value={this.state.textAreaInput}
+          onChange={this.handleTextArea}
+        />
+
+        {!this.state.isValid ? this.renderError() : null}
+
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      inputValue: '',
+      remainingChars: this.props.maxChars
+    };
+  }
+
+  handleTextChange = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    }, ()=>{
+      this.setState({
+        remainingChars: this.state.remainingChars - this.state.inputValue.length
+      })
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+      <input type="text" value={this.state.inputValue} onChange={this.handleTextChange} />
+    <p>{this.state.remainingChars}</p>
       </div>
     );
   }
